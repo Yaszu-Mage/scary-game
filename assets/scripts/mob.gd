5d22137c9e50098043c1f18f76bde0706a256452
Yaszu <76853160+Yaszu-Mage@users.noreply.github.com>
2024-10-24 11:14:38 -0700

    made wander code @ wallrun start code

--- assets/scripts/mob.gd
+++ assets/scripts/mob.gd
@@ -2,11 +2,17 @@ extends CharacterBody3D
 
 @onready var agent = $NavigationAgent3D
 @onready var timer = $Timer
+var spawn_pos : Vector3 = Vector3.ZERO
 var movement_delta: float
+var health = 100
+var last_bod_pos : Vector3 = Vector3.ZERO
 var bod
-const SPEED = 5.0
+const SPEED = 2.5
 var compvel
 const JUMP_VELOCITY = 4.5
+func _ready():
+	spawn_pos = self.global_position
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -30,7 +36,10 @@ func comp_agent():
 		agent.target_position.x = bod.global_position.x
 		agent.target_position.z = bod.global_position.z
 	else:
-		pass
+		if last_bod_pos != Vector3.ZERO:
+			agent.target_position = last_bod_pos
+			if round(last_bod_pos) == round(self.global_position):
+				wander()
 
 func _on_navigation_agent_3d_velocity_computed(safe_velocity):
 	global_position = global_position.move_toward(global_position + safe_velocity, movement_delta)
@@ -46,3 +55,15 @@ func _on_timer_timeout():
 
 func _on_area_3d_body_exited(body):
 	bod = null
+
+
+func wander():
+	agent.target_position = Vector3(self.global_position.x + randf_range(1,5),self.global_position.y,self.global_position.z + randf_range(1,5))
+
+func spawn():
+	agent.target_position = spawn_pos
+	
+	
+func respawn():
+	if health == 0:
+		self.global_position = spawn_pos
